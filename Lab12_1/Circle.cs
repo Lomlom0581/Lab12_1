6e2b42f8972cd68f384c9d3e14971d0c89634409
Антон Еремеев <kzanton_81@mail.ru>
2021-12-25 22:17:22 +0300

    Добавьте файлы проекта.

---
+++ Lab12_1/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12_1
+{
+    public static class Circle
+    {        
+        public static double GetLength(double radius) => 2 * Math.PI * radius;
+                
+        public static double GetArea(double radius) => Math.PI * Math.Pow(radius, 2);
+                
+        public static bool IsPointInCircle(double x, double y, double radius) => Math.Sqrt(x * x + y * y) <= radius;
+
+    }
+}
---
+++ Lab12_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет! Программа проверяет принадлежность точки с координатами (x,y) кругу с радиусом r и координатами центра x0, y0 ");
+            Console.Write("Введите радиус окружности :");
+            double r = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите координату точки по оси абсцисс:");
+            double x = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите координату точки по оси ординат:");
+            double y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Длинна окружности:{Circle.GetLength(r)}");
+            Console.WriteLine($"Прощадь окружности:{Circle.GetArea(r)}");
+
+            if (Circle.IsPointInCircle(x, y, r))
+                Console.Write("Точка пренадлежит окружности");
+            else
+                Console.Write("Точка НЕ пренадлежит окружности");
+
+            Console.ReadKey();
+        }
+    }
+}
